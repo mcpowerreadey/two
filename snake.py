338dbd492d3bb9f6b1711b48d9bf8803cade1e76
mcpowerreadey <160994321+mcpowerreadey@users.noreply.github.com>
2024-03-10 17:20:34 +0800

    Update.py

--- snake.py
+++ snake.py
@@ -1,113 +1,44 @@
-import pygame
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
-import random
 
-pygame.init()
+# 设置指纹浏览器的选项
+options = webdriver.ChromeOptions()
+options.add_argument("--start-maximized")  # 最大化窗口
+options.add_argument("--ignore-certificate-errors")  # 忽略证书错误
+options.add_argument("--disable-popup-blocking")  # 禁用弹出窗口阻止功能
 
-# Define colors
-white = (255, 255, 255)
-yellow = (255, 255, 102)
-black = (0, 0, 0)
-red = (213, 50, 80)
-green = (0, 255, 0)
-blue = (50, 153, 213)
+# 启动指纹浏览器
+browser = webdriver.Chrome(options=options)
 
-# Display window size
-dis_width = 600
-dis_height = 400
-dis = pygame.display.set_mode((dis_width, dis_height))
+# 打开搜索引擎进行搜索
+search_query = "Python automation with Selenium"  # 要搜索的关键词
+search_engine_url = "https://www.google.com"  # 替换为你想使用的搜索引擎
+browser.get(search_engine_url)
+search_box = browser.find_element_by_name("q")  # 根据搜索引擎的网页结构找到搜索框元素
+search_box.send_keys(search_query + Keys.RETURN)  # 输入搜索关键词并按下回车键
 
-clock = pygame.time.Clock()
+time.sleep(5)  # 等待页面加载
 
-snake_block = 10
-snake_speed = 15
+# 登录Twitter
+twitter_url = "https://twitter.com/login"  # Twitter登录页面
+browser.get(twitter_url)
+username = "your_username"  # 替换为你的Twitter用户名
+password = "your_password"  # 替换为你的Twitter密码
+username_field = browser.find_element_by_name("session[username_or_email]")
+password_field = browser.find_element_by_name("session[password]")
+login_button = browser.find_element_by_xpath('//span[text()="Log in"]')
 
-font_style = pygame.font.SysFont("bahnschrift", 25)
+username_field.send_keys(username)
+password_field.send_keys(password)
+login_button.click()
 
-def our_snake(snake_block, snake_list):
-    for x in snake_list:
-        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
+time.sleep(5)  # 等待登录完成
 
-def message(msg, color):
-    mesg = font_style.render(msg, True, color)
-    dis.blit(mesg, [dis_width / 6, dis_height / 3])
+# 在这里可以进行后续操作，例如发推、查看时间线等
+# 例如：
+# tweet_box = browser.find_element_by_xpath('//div[@aria-label="Tweet text"]')
+# tweet_box.send_keys('Hello, this is a tweet from automated script!')
 
-def gameLoop():
-    game_over = False
-    game_close = False
-
-    x1 = dis_width / 2
-    y1 = dis_height / 2
-
-    x1_change = 0
-    y1_change = 0
-
-    snake_List = []
-    Length_of_snake = 1
-
-    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-    foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
-
-    while not game_over:
-
-        while game_close == True:
-            dis.fill(blue)
-            message("You lost! Press Q-Quit or C-Play Again", red)
-            pygame.display.update()
-
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_q:
-                        game_over = True
-                        game_close = False
-                    if event.key == pygame.K_c:
-                        gameLoop()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game_over = True
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    x1_change = -snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_RIGHT:
-                    x1_change = snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_UP:
-                    y1_change = -snake_block
-                    x1_change = 0
-                elif event.key == pygame.K_DOWN:
-                    y1_change = snake_block
-                    x1_change = 0
-
-        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
-            game_close = True
-        x1 += x1_change
-        y1 += y1_change
-        dis.fill(blue)
-        pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
-        snake_Head = []
-        snake_Head.append(x1)
-        snake_Head.append(y1)
-        snake_List.append(snake_Head)
-        if len(snake_List) > Length_of_snake:
-            del snake_List[0]
-
-        for x in snake_List[:-1]:
-            if x == snake_Head:
-                game_close = True
-
-        our_snake(snake_block, snake_List)
-        pygame.display.update()
-
-        if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
-            Length_of_snake += 1
-
-        clock.tick(snake_speed)
-
-    pygame.quit()
-    quit()
-
-gameLoop()
+# 关闭指纹浏览器
+browser.quit()
